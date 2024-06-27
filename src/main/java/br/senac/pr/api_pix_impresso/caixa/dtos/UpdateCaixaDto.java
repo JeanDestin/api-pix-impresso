ddef8d5d7735c35e33a789373f45874310b1b198
Jean Destin <destin.2206@aluno.pr.senac.br>
2024-06-27 20:04:16 -0300

    commit inicial

---
+++ src/main/java/br/senac/pr/api_pix_impresso/caixa/dtos/UpdateCaixaDto.java
@@ -0,0 +1,27 @@
+package br.senac.pr.api_pix_impresso.caixa.dtos;
+
+public class UpdateCaixaDto {
+    private String localizacao;
+    private Double saldo;
+
+    public UpdateCaixaDto(String localizacao, Double saldo) {
+        this.localizacao = localizacao;
+        this.saldo = saldo;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
